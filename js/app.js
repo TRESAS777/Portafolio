c7eaf96a2564401ee9c24b1c6fd4ed5108aafb4b
TRESAS777 <103174712+TRESAS777@users.noreply.github.com>
2023-12-19 08:09:12 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -11,3 +11,47 @@ const cambiarTema = () => {
     document.querySelector("body").getAttribute("data-bs-theme") === "light" ? 
     temaOscuro() : temaClaro();
 }
+
+///////////////////////////////////////////////////////////////////////
+let textoAnimado = document.querySelector("#nombre");
+
+    textoAnimado.innerHTML=textoAnimado.innerHTML.split("").map((letra,index) => `<span style="transition-delay: ${index * 40}ms; filter: hue-rotate(${index * 40}deg);" >${letra}</span>`).join("")
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     direction: 'vertical',
+//     loop: true,
+  
+//     // If we need pagination
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },
+  
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+  
+//     // And if we need scrollbar
+//     scrollbar: {
+//       el: '.swiper-scrollbar',
+//     },
+//   });
+  
